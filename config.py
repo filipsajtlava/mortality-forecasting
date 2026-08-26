e4d50b209c8769042ef60762808f89007f353867
filipsajtlava <sajtlavafilip@gmail.com>
2026-08-26 16:23:20 +0200

    Minimal test suite for model utilizing StMoMo data for benchmarking

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
-from dataclasses import dataclass
-
 DATA_DIRECTORY_NAME = "hmd_data"
 STALE_FALLBACK_COUNTRY_AVAILABLE = "supported_countries_fallback.json"
 CACHED_COUNTRY_AVAILABLE = "countries_available.json"
@@ -7,7 +5,7 @@ CACHED_TTL_COUNTRY_CODE_DAYS = 360
 CACHED_TTL_COUNTRY_DATA_DAYS = 360
 
 FILE_SELECTION_COUNTRY_DATA = {
-    "M": "Mx_1x1.txt",
+    "M": "Mortalities_1x1.txt",
     "E": "Exposures_1x1.txt",
     "D": "Deaths_1x1.txt"
 }
---
+++ tests/benchmarks/generate_stmomo_benchmark.R
@@ -0,0 +1,62 @@
+library(StMoMo)
+library(dplyr)
+library(here)
+library(jsonlite)
+library(stringr)
+
+data_dir <- here("tests", "benchmarks", "data")
+output_dir <- here("tests", "benchmarks", "outputs")
+
+data(EWMaleData)
+write.csv(EWMaleData$Dxt, file.path(data_dir, "GBRTENW_Deaths.csv"))
+write.csv(EWMaleData$Ext, file.path(data_dir, "GBRTENW_Exposures.csv"))
+write.csv(
+    EWMaleData$Dxt / EWMaleData$Ext, 
+    file.path(data_dir, "GBRTENW_Mortalities.csv")
+)
+
+data_files <- list.files(data_dir)
+available_countries <- unique(str_match(data_files, "^(.+)_")[, 2])
+
+for (country_code in available_countries) {
+        deaths_path <- file.path(
+            data_dir, 
+            paste0(country_code, "_Deaths.csv")
+        )
+        exposures_path <- file.path(
+            data_dir, 
+            paste0(country_code, "_Exposures.csv")
+        )
+    
+    deaths_mat <- as.matrix(read.csv(
+        deaths_path, row.names = 1, check.names = FALSE
+    ))
+    expos_mat <- as.matrix(read.csv(
+        exposures_path, row.names = 1, check.names = FALSE
+    ))
+
+    stmomo_data <- structure(
+        list(
+            Dxt = deaths_mat,
+            Ext = expos_mat,
+            ages = as.numeric(rownames(deaths_mat)),
+            years = as.numeric(colnames(deaths_mat)),
+            type = "central"
+        ),
+        class = "StMoMoData"
+    )
+
+    lc_model <- lc(link = "log", const = "sum")
+    fit_lc <- fit(lc_model, data = stmomo_data)
+    estimated_parameters <- list(
+        ax = as.vector(fit_lc$ax),
+        bx = as.vector(fit_lc$bx),
+        kt = as.vector(fit_lc$kt)
+    )
+
+    write_json(
+        estimated_parameters, 
+        file.path(output_dir, paste0(country_code, "_estimated_params.json")), 
+        pretty = TRUE
+    )
+}
